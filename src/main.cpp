213f90a1746c0a7e58c5ee2b26ba398518b61bae
jPPuetz <philipp-puetz@t-online.de>
2015-10-31 15:10:41 +0100

    Added wall object and new textures

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "engine/texturemanager.hpp"
 #include "gameobjects/ball.hpp"
 #include "gameobjects/character.hpp"
+#include "gameobjects/wall.hpp"
 
 #include <iostream>
 #include <memory>
@@ -13,25 +14,43 @@ using namespace theseus::gameobjects;
 
 int main()
 {
-    cout << "Hello." << endl;
+	cout << "Hello." << endl;
 
-    // Load the game
-    theseus::engine::Game game;
+	// Load the game
+	theseus::engine::Game game;
 
-    // Create the first scene
-    unique_ptr<Scene> initScene = unique_ptr<Scene>(new Scene());
+	// Create the first scene
+	unique_ptr<Scene> initScene = unique_ptr<Scene>(new Scene());
 
-    // load the textures
-    TextureManager::instance().loadTexture("player.png");
+	// load the textures
+	TextureManager::instance().loadTexture("player.png");
+	TextureManager::instance().loadTexture("wall_horizontal.png");
+	TextureManager::instance().loadTexture("wall_vertical.png");
+	TextureManager::instance().loadTexture("wall_edge_left_bottom.png");
+	TextureManager::instance().loadTexture("wall_edge_left_top.png");
+	TextureManager::instance().loadTexture("wall_edge_right_bottom.png");
+	TextureManager::instance().loadTexture("wall_edge_right_top.png");
+	TextureManager::instance().loadTexture("wall_left_middle.png");
+	TextureManager::instance().loadTexture("wall_left_end.png");
+	TextureManager::instance().loadTexture("wall_right_middle.png");
+	TextureManager::instance().loadTexture("wall_right_end.png");
+	TextureManager::instance().loadTexture("wall_bottom_end.png");
+	TextureManager::instance().loadTexture("wall_top_end.png");
+	TextureManager::instance().loadTexture("wall_cross.png");
+	TextureManager::instance().loadTexture("wall_T_cross.png");
+	TextureManager::instance().loadTexture("wall_T_upsidedown_cross.png");
 
-    // Populate it with some game objects
-    auto man = unique_ptr<Character>(new Character);
-    initScene->addGameObject(move(man));
+	// Populate it with some game objects
+	auto wall = unique_ptr<Wall>(new Wall(1, sf::Vector2f(50, 50), sf::Vector2f(320, 64)));
+	initScene->addGameObject(move(wall));
 
-    // Start the game with that scene
-    game.run(move(initScene));
+	auto man = unique_ptr<Character>(new Character);
+	initScene->addGameObject(move(man));
 
-    // end
-    TextureManager::reset();
-    return 0;
+	// Start the game with that scene
+	game.run(move(initScene));
+
+	// end
+	TextureManager::reset();
+	return 0;
 }
