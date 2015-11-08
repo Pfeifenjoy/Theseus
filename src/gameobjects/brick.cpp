4dad20e4f343e3f7cabe6d73887afede831a7c89
Arwed Mett <arwed.mett@googlemail.com>
2015-11-08 19:55:15 +0100

    fixed collision bug

--- src/gameobjects/brick.cpp
+++ src/gameobjects/brick.cpp
@@ -43,30 +43,30 @@ void Brick::setType(BrickType brickType) {
 		size = sf::Vector2f(26,64);
 		sprite(2).setPosition(sf::Vector2f(6,0));
 		topLeft = sf::Vector2f(0, figureOffset);
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		break;
 	case EDGE_LEFT_BOTTOM:
 		setTexture(2, TextureManager::instance().getTexture("wall_edge_left_bottom.png"));
 		size = sf::Vector2f(26,64);
 		sprite(2).setPosition(sf::Vector2f(6,0));
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		break;
 	case EDGE_RIGHT_BOTTOM:
 		setTexture(2, TextureManager::instance().getTexture("wall_edge_right_bottom.png"));
 		size = sf::Vector2f(26,64);
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		break;
 	case EDGE_RIGHT_TOP:
 		setTexture(2, TextureManager::instance().getTexture("wall_edge_right_top.png"));
 		size = sf::Vector2f(26,64);
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		topLeft = sf::Vector2f(0, figureOffset);
 		break;
 	case LEFT_MIDDLE:
 		setTexture(2, TextureManager::instance().getTexture("wall_left_middle.png"));
 		size = sf::Vector2f(26,64);
 		sprite(2).setPosition(sf::Vector2f(6,0));
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		break;
 	case LEFT_END:
 		setTexture(2, TextureManager::instance().getTexture("wall_left_end.png"));
@@ -77,7 +77,7 @@ void Brick::setType(BrickType brickType) {
 	case RIGHT_MIDDLE:
 		setTexture(2, TextureManager::instance().getTexture("wall_right_middle.png"));
 		size = sf::Vector2f(26,64);
-		bottomRight = size;
+		bottomRight = sf::Vector2f(20,64);
 		break;
 	case RIGHT_END:
 		setTexture(2, TextureManager::instance().getTexture("wall_right_end.png"));
